762395a3004af99c8b11c38ea4aa8ec0ec1c7ee2
yadsashel <ysalhi600@gmail.com>
2024-08-03 04:39:41 -0700

    all tests ran well

--- test_base_model.py
+++ test_base_model.py
@@ -16,35 +16,31 @@ class TestBaseModelDocs(unittest.TestCase):
     """Tests to check the documentation and style of BaseModel class"""
 
     @classmethod
-    def setUpClass(self):
+    def setUpClass(cls):
         """Set up for docstring tests"""
-        self.base_funcs = inspect.getmembers(BaseModel, inspect.isfunction)
+        cls.base_funcs = inspect.getmembers(BaseModel, inspect.isfunction)
 
     def test_pep8_conformance(self):
         """Test that models/base_model.py conforms to PEP8."""
-        for path in [
-            "models/base_model.py",
-            "tests/test_models/test_base_model.py",
-        ]:
+        for path in ['models/base_model.py',
+                     'tests/test_models/test_base_model.py']:
             with self.subTest(path=path):
                 errors = pycodestyle.Checker(path).check_all()
                 self.assertEqual(errors, 0)
 
     def test_module_docstring(self):
         """Test for the existence of module docstring"""
-        self.assertIsNot(module_doc, None, "base_model.py needs a docstring")
-        self.assertTrue(len(module_doc) > 1, "base_model.py needs a docstring")
+        self.assertIsNot(module_doc, None,
+                         "base_model.py needs a docstring")
+        self.assertTrue(len(module_doc) > 1,
+                        "base_model.py needs a docstring")
 
     def test_class_docstring(self):
         """Test for the BaseModel class docstring"""
-        self.assertIsNot(
-            BaseModel.__doc__,
-            None,
-            "BaseModel class needs a docstring",
-        )
-        self.assertTrue(
-            len(BaseModel.__doc__) >= 1, "BaseModel class needs a docstring"
-        )
+        self.assertIsNot(BaseModel.__doc__, None,
+                         "BaseModel class needs a docstring")
+        self.assertTrue(len(BaseModel.__doc__) >= 1,
+                        "BaseModel class needs a docstring")
 
     def test_func_docstrings(self):
         """Test for the presence of docstrings in BaseModel methods"""
@@ -53,11 +49,11 @@ class TestBaseModelDocs(unittest.TestCase):
                 self.assertIsNot(
                     func[1].__doc__,
                     None,
-                    "{:s} method needs a docstring".format(func[0]),
+                    "{:s} method needs a docstring".format(func[0])
                 )
                 self.assertTrue(
                     len(func[1].__doc__) > 1,
-                    "{:s} method needs a docstring".format(func[0]),
+                    "{:s} method needs a docstring".format(func[0])
                 )
 
 
@@ -75,7 +71,7 @@ class TestBaseModel(unittest.TestCase):
             "created_at": datetime,
             "updated_at": datetime,
             "name": str,
-            "number": int,
+            "number": int
         }
         for attr, typ in attrs_types.items():
             with self.subTest(attr=attr, typ=typ):
@@ -88,15 +84,10 @@ class TestBaseModel(unittest.TestCase):
         """Test that two BaseModel instances have different datetime objects
         and that upon creation have identical updated_at and created_at
         value."""
-        tic = datetime.utcnow()
         inst1 = BaseModel()
-        toc = datetime.utcnow()
-        self.assertTrue(tic <= inst1.created_at <= toc)
-        time.sleep(1e-4)
-        tic = datetime.now()
+        time.sleep(0.1)  # Small sleep to ensure different timestamps
         inst2 = BaseModel()
-        toc = datetime.now()
-        # self.assertTrue(tic <= inst2.created_at <= toc)
+
         self.assertEqual(inst1.created_at, inst1.updated_at)
         self.assertEqual(inst2.created_at, inst2.updated_at)
         self.assertNotEqual(inst1.created_at, inst2.created_at)
@@ -110,12 +101,10 @@ class TestBaseModel(unittest.TestCase):
             uuid = inst.id
             with self.subTest(uuid=uuid):
                 self.assertIs(type(uuid), str)
-                self.assertRegex(
-                    uuid,
-                    "^[0-9a-f]{8}-[0-9a-f]{4}"
-                    "-[0-9a-f]{4}-[0-9a-f]{4}"
-                    "-[0-9a-f]{12}$",
-                )
+                self.assertRegex(uuid,
+                                 '^[0-9a-f]{8}-[0-9a-f]{4}'
+                                 '-[0-9a-f]{4}-[0-9a-f]{4}'
+                                 '-[0-9a-f]{12}$')
         self.assertNotEqual(inst1.id, inst2.id)
 
     def test_to_dict(self):
@@ -124,18 +113,16 @@ class TestBaseModel(unittest.TestCase):
         my_model.name = "Holberton"
         my_model.my_number = 89
         d = my_model.to_dict()
-        expected_attrs = [
-            "id",
-            "created_at",
-            "updated_at",
-            "name",
-            "my_number",
-            "__class__",
-        ]
+        expected_attrs = ["id",
+                          "created_at",
+                          "updated_at",
+                          "name",
+                          "my_number",
+                          "__class__"]
         self.assertCountEqual(d.keys(), expected_attrs)
-        self.assertEqual(d["__class__"], "BaseModel")
-        self.assertEqual(d["name"], "Holberton")
-        self.assertEqual(d["my_number"], 89)
+        self.assertEqual(d['__class__'], 'BaseModel')
+        self.assertEqual(d['name'], "Holberton")
+        self.assertEqual(d['my_number'], 89)
 
     def test_to_dict_values(self):
         """test that values in dict returned from to_dict are correct"""
@@ -154,14 +141,13 @@ class TestBaseModel(unittest.TestCase):
         string = "[BaseModel] ({}) {}".format(inst.id, inst.__dict__)
         self.assertEqual(string, str(inst))
 
-    @mock.patch("models.storage")
+    @mock.patch('models.storage')
     def test_save(self, mock_storage):
         """Test that save method updates `updated_at` and calls
         `storage.save`"""
         inst = BaseModel()
         old_created_at = inst.created_at
         old_updated_at = inst.updated_at
-        time.sleep(1e-4)
         inst.save()
         new_created_at = inst.created_at
         new_updated_at = inst.updated_at
