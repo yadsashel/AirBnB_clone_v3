3ff86c2a162caf7964c8ecb919e3ab1554b6b44e
yadsashel <ysalhi600@gmail.com>
2024-08-05 04:03:28 -0700

    Add stats endpoint to retrieve the number of each object type

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
-The RESTful api starts here. The api aids data access in the app.
+The RESTful API starts here. The API aids data access in the app.
 """
 from os import getenv
-
 from flask import Flask, jsonify
 from flask_cors import CORS
-
 from api.v1.views import app_views
 from models import storage
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.register_blueprint(app_views)
-CORS(app, origins=["0.0.0.0"])
+app.register_blueprint(app_views)  # Register the blueprint
+CORS(app, origins=["0.0.0.0"])  # Set up CORS
+
+
 host = getenv("HBNB_API_HOST", "0.0.0.0")
 port = getenv("HBNB_API_PORT", "5000")
 
@@ -30,4 +30,4 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    app.run(host, port, threaded=True, debug=True)
+    app.run(host=host, port=int(port), threaded=True, debug=True)
